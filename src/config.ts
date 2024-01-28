ae90b9ca8e7a1b6d42fcbe8a03629c992e7c1861
Marcel267 <martinez.otano2000@web.de>
2024-01-28 17:38:52 +0100

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Marcel Martinez Otano";
+export const SITE_DESCRIPTION =
+	"Kleines Portfolio, gebaut mit Astro und TailwindCSS.";
+export const TRANSITION_API = true;
