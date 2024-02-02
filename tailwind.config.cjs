c5b3feb6fa5b534fb441a7e8d2c89f4c14a0f6f3
Marcel267 <martinez.otano2000@web.de>
2024-02-02 21:12:57 +0100

    add text to projects

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/*/.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
@@ -8,6 +8,29 @@ module.exports = {
 	daisyui: {
 		themes: ['dracula'], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dark", // name of one of the included themes for dark mode
+		// themes: [
+		// 	{
+		// 	  mytheme: {
+		// 		"primary": "#f44336",
+		// 		"secondary": "#f6d860",
+		// 		"accent": "#37cdbe",
+		// 		"neutral": "#3d4451",
+		// 		"base-100": "#ffffff",
+		// 	  },
+		// 	},
+		//   ],
+
+		// themes: [
+		// 	{
+		// 	  mytheme: {
+		// 		...require("daisyui/src/theming/themes")["dracula"],
+		// 		primary: "#7480ff",
+		// 		secondary: "#ff52d9",
+		// 	  },
+		// 	},
+		//   ],
+		//   darkTheme: 'mytheme',
+
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	}
 }
