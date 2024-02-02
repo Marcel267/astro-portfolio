145acb19536aafdb5cb1c97ec3294f7c8a417250
Marcel267 <martinez.otano2000@web.de>
2024-02-02 21:16:50 +0100

    fix tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/*/.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
@@ -8,6 +8,7 @@ module.exports = {
 	daisyui: {
 		themes: ['dracula'], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dark", // name of one of the included themes for dark mode
+		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 		// themes: [
 		// 	{
 		// 	  mytheme: {
@@ -30,7 +31,5 @@ module.exports = {
 		// 	},
 		//   ],
 		//   darkTheme: 'mytheme',
-
-		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	}
 }
