f5aea4bc0e64166a9a5673fc2c4c77a26bc28557
Marcel267 <martinez.otano2000@web.de>
2024-02-11 22:06:12 +0100

    try fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mmotano.de',
-  base: '/astro-portfolio',
+  // base: '/astro-portfolio',
   integrations: [sitemap(), tailwind()]
 });
